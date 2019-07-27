efd85be859ce39a3b280d2edff5a0ea6dc34ddec
Vixson <vixsonvx@gmail.com>
2019-07-27 20:20:16 +0100

    Completed Presskit
    1. Updated all Footer style.
    2. Updated color variables.
    3. Updated Icons and Logos.
    4. Added all presskit download files.
    5. Other fixes and updates...

---
+++ assets/img/colors.swift
@@ -0,0 +1,3 @@
+let primaryColor = UIColor(red: 0.01, green: 0.11, blue: 0.31, alpha: 1.0);
+let secondaryColor = UIColor(red: 0.96, green: 0.79, blue: 0.31, alpha: 1.0);
+let darkColor = UIColor(red: 0.01, green: 0.01, blue: 0.01, alpha: 1.0);
